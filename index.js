14bce98adb97685e04547792e1014fe8573d7e43
JinweiOS <jinwei.peng@beingthink.com>
2023-09-23 16:33:50 +0800

    feat: koa body引入

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import Koa from 'koa'
 import router from './src/router.js'
 import { valid } from './src/middleware/valid.js'
+import { koaBody } from 'koa-body'
 
 // 1. server即为文档中的application对象
 const server = new Koa()
 
 // 一、验证用户是否合法
 server.use(valid)
+server.use(koaBody({
+  multipart: true,
+  formidable: {
+    uploadDir: `${process.cwd()}/upload`,
+    maxFieldsSize: 2 * 1024 * 1024 * 1024, // 2gb
+    keepExtensions: true,
+    onFileBegin: (name, file) => {
+      // 生成新的文件名
+      const fileName = `tuchuang_${file.newFilename}`
+
+      // filepath:  c:/user/dalina/project/beingthink/learnKoa03/upload/00003223423432r23.png
+      // file.newFilename
+      file.filepath = `${file.filepath.replace(file.newFilename, fileName)}`;
+    },
+  }
+}))
 
 // 二、处理业务逻辑
 server.use(router.routes()).use(router.allowedMethods())
--- src/router.js
+++ src/router.js
@@ -6,6 +6,19 @@ router.get('/', (ctx) => {
   ctx.body = 'hello world'
 })
 
+router.post('/:userId', (ctx) => {
+  console.log(ctx)
+  ctx.body = 'hello world'
+  console.log(ctx.header)
+  console.log(ctx.query)
+  console.log(ctx.params)
+  console.log(ctx.request.body)
+  console.log(ctx.request.files)
+  ctx.req.on('data', (chunk) => {
+    console.log(chunk.toString())
+  })
+})
+
 router.post('/login', (ctx) => {
   ctx.body = 'login'
 })
